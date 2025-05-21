5fb33cc6604d655817ff6f3b5e30ee2d64aa569e
Regina <regina.hernandezrdr@uanl.edu.mx>
2025-05-21 12:45:07 -0600

    Initial commit

---
+++ src/main/java/org/example/saludexpress/SaludExpressApplication.java
@@ -0,0 +1,13 @@
+package org.example.saludexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/saludexpress/SaludExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.saludexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
